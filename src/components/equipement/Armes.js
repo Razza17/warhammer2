c3099473093265b423001e9fd5a51b50437a65ce
Fabien Cordon <cordon.fabien@gmail.com>
2017-10-17 14:01:24 +0200

    ArmesTable improvement

--- src/components/equipement/Armes.js
+++ src/components/equipement/Armes.js
@@ -8,7 +8,7 @@ export class Armes extends Component {
                 <td>{this.props.encombrement}</td>
                 <td>{this.props.degats}</td>
                 <td>{this.props.portee}</td>
-                <td>{this.props.recharge}</td>
+                <td>{this.props.recharge !== "" ? this.props.recharge : 0}</td>
                 <td>{this.props.attribut}</td>
             </tr>
         )
