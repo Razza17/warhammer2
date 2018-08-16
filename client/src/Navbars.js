5d2c818c7c4b2f01a5b25947f63d39e389f637bb
Fabien Cordon <f.cordon@cheil.com>
2018-08-16 11:47:30 +0200

    Fix show/hide nav issue

--- client/src/Navbars.js
+++ client/src/Navbars.js
@@ -1,86 +1,16 @@
 import React, { Component }  from 'react';
-import { NavLink } from 'react-router-dom';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import { Navbar } from 'react-bootstrap';
 
 export class Navbars extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: "undefined",
-      perso: "undefined",
-      showNav: false
-    }
-  }
-
-  componentWillReceiveProps (nextProps, nextState) {
-    let user;
-    let perso;
-
-    if (window.location.pathname === "/personnage" || window.location.pathname === "/equipement" || window.location.pathname === "/combat" || window.location.pathname === "/moncompte") {
-      let urlParams = window.location.search.substring(1).split('=');
-      let recupUser = urlParams[1].split('&');
-      user = recupUser[0];
-      perso = urlParams[2];
-      this.setState({user: user, perso: perso});
-    }
-
-    user !== undefined && perso !== undefined ? this.setState({showNav: !this.state.showNav}) : this.setState({showNav: false})
-  }
-
-  logout() {
-    let config = {
-      apiKey: "AIzaSyCXSmiyYCqx8LWXeC16RoBFo-j0Kvlnx-Q",
-      authDomain: "warhammer-81ced.firebaseapp.com",
-      databaseURL: "https://warhammer-81ced.firebaseio.com",
-      projectId: "warhammer-81ced",
-      storageBucket: "warhammer-81ced.appspot.com",
-      messagingSenderId: "1046515260577"
-    };
-    firebase.initializeApp(config);
-
-    firebase.auth().signOut().then(function() {
-      window.location.assign("/")
-    }).catch(function(error) {
-      // An error happened.
-    });
-  }
-
   render() {
     return (
       <Navbar collapseOnSelect>
         <Navbar.Header>
-          <Navbar.Brand>
-            <NavLink to={"/?pseudo="+this.state.user+"&perso="+this.state.perso} className="navbar-brand">WARHAMMER 2.0</NavLink>
+          <Navbar.Brand className="navbar-brand">
+            WARHAMMER 2.0
           </Navbar.Brand>
           <Navbar.Toggle id='collapseButton' />
         </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav className={this.state.showNav ? "show" : "hide"}>
-            <LinkContainer to={"/personnage?pseudo="+this.state.user+"&perso="+this.state.perso}>
-              <NavItem eventKey={0}>personnage</NavItem>
-            </LinkContainer>
-            <LinkContainer to={"/equipement?pseudo="+this.state.user+"&perso="+this.state.perso}>
-              <NavItem eventKey={1}>Equipement</NavItem>
-            </LinkContainer>
-            <LinkContainer to={"/combat?pseudo="+this.state.user+"&perso="+this.state.perso}>
-              <NavItem eventKey={2}>Combat</NavItem>
-            </LinkContainer>
-          </Nav>
-          <Nav className={this.state.showNav ? "show" : "hide"} pullRight>
-            <NavItem eventKey={3} href="#">
-              Mon Compte
-            </NavItem>
-            <NavItem eventKey={4} onClick={this.logout.bind(this)}>
-              Me déconnecter
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
       </Navbar>
     )
   }
--- client/src/hocs/authFunction.js
+++
@@ -1,9 +0,0 @@
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(res) {
-    res === "true" ? this.isAuthenticated = "true" : this.isAuthenticated = "false"
-    return this.isAuthenticated;
-  }
-}
-
-export default fakeAuth;
